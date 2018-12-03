85f06d83edd2568a2b1c75717413bdc83e723c3a
Lauren Tan <laurent@netflix.com>
2018-12-02 16:28:26 -0800

    Refactor day 1 part 1

--- 2018/day-01/src/main.rs
+++ 2018/day-01/src/main.rs
@@ -1,30 +1,20 @@
+use std::io;
 use std::fs;
-use std::io::Cursor;
-use std::io::prelude::*;
 
-fn print_report(curr: i32, change: String, next: i32) {
-    println!(
-        "Current frequency {curr}, change of {change}; resulting frequency {next}",
-        curr = curr,
-        change = change,
-        next = next
-    );
+type Frequency = i32;
+
+fn part_one(init_freq: Frequency, input: &str) -> io::Result<()> {
+    let res = fs::read_to_string(&input)
+        .unwrap()
+        .lines()
+        .filter_map(|l| l.parse::<Frequency>().ok())
+        .fold(init_freq, |acc, curr| acc + curr);
+    println!("Part 1: Result is {}", res);
+    Ok(())
 }
 
-fn cursor_from_file(path: &'static str) -> Cursor<String> {
-    let contents = fs::read_to_string(path).unwrap();
-    return Cursor::new(contents);
-}
-
-fn main() -> std::io::Result<()> {
-    let mut curr_freq: i32 = 0;
-    let lines_iter = cursor_from_file("INPUT").lines().map(|l| l.unwrap());
-    for l in lines_iter {
-        let change = l.parse::<i32>().unwrap();
-        let next_freq = curr_freq + change;
-        print_report(curr_freq, l, next_freq);
-        curr_freq = next_freq;
-    }
-    println!("Result is {}", curr_freq);
+fn main() -> io::Result<()> {
+    let input = String::from("INPUT");
+    part_one(0, &input)?;
     Ok(())
 }
